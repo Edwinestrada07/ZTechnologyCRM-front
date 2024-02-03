7221bcfb8c907bfc4c2e2b2b12786d7737cd382a
Edwinestrada07 <r.1993andres.ee@gmail.com>
2024-02-03 06:49:00 -0500

    Corrección de errores

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -4,16 +4,11 @@ function Cart() {
 
     return ( 
       <>
-<<<<<<< HEAD
+
         <div className="text-white p-5 vh-100">
 
             <h2 className="text-center font-weight-normal">Página de Cotizaciones</h2>
-=======
-        <div className="container-fluid bg-dark text-white vh-100 p-5">
-
-            <h2 className="text-center">Página de Cotizaciones</h2>
->>>>>>> 06bc34000890fb1a6bc307742af4e37b6faa59fe
-            
+  
         </div>
       </>
     )
